101c364700ab0333848346ef9fdedfa1658e89a0
xiaoranzhang11 <xiaoranzhang11@gmail.com>
2024-10-05 16:50:32 +0000

    changes for Dublin competition 2024/10/5

--- autonomousPart1.py
+++ autonomousPart1.py
@@ -14,20 +14,6 @@ Conveyor_motor_a = Motor(Ports.PORT8, True)
 Conveyor_motor_b = Motor(Ports.PORT2, False)
 Conveyor = MotorGroup(Conveyor_motor_a, Conveyor_motor_b)
 
-
-
-# generating and setting random seed
-def initializeRandomSeed():
-    wait(100, MSEC)
-    xaxis = brain_inertial.acceleration(XAXIS) * 1000
-    yaxis = brain_inertial.acceleration(YAXIS) * 1000
-    zaxis = brain_inertial.acceleration(ZAXIS) * 1000
-    systemTime = brain.timer.system() * 100
-    urandom.seed(int(xaxis + yaxis + zaxis + systemTime)) 
-    
-# Initialize random seed 
-initializeRandomSeed()
-
 #endregion VEXcode Generated Robot Configuration
 
 import time
@@ -107,7 +93,7 @@ def Arc_Forward_Distance_Velocity_Offset_Direction(Arc_Forward_Distance_Velocity
 def GyroTurnV2_Heading_Velocity_Momentum(GyroTurnV2_Heading_Velocity_Momentum__Heading, GyroTurnV2_Heading_Velocity_Momentum__Velocity, GyroTurnV2_Heading_Velocity_Momentum__Momentum):
     global LeftDriveSpeed, RightDriveSpeed, ShootingSpeed, message1
     LeftDrive.set_velocity(GyroTurnV2_Heading_Velocity_Momentum__Velocity, PERCENT)
-    LeftDrive.set_velocity(GyroTurnV2_Heading_Velocity_Momentum__Velocity, PERCENT)
+    RightDrive.set_velocity(GyroTurnV2_Heading_Velocity_Momentum__Velocity, PERCENT)
     while (gyro_11.heading() < GyroTurnV2_Heading_Velocity_Momentum__Heading - GyroTurnV2_Heading_Velocity_Momentum__Momentum or gyro_11.heading() > GyroTurnV2_Heading_Velocity_Momentum__Heading + GyroTurnV2_Heading_Velocity_Momentum__Momentum):
         if gyro_11.heading() < GyroTurnV2_Heading_Velocity_Momentum__Heading - GyroTurnV2_Heading_Velocity_Momentum__Momentum:
             LeftDrive.spin(FORWARD)
@@ -115,7 +101,7 @@ def GyroTurnV2_Heading_Velocity_Momentum(GyroTurnV2_Heading_Velocity_Momentum__H
         else:
             LeftDrive.spin(REVERSE)
             RightDrive.spin(FORWARD)
-        wait(20, MSEC)
+        wait(10, MSEC)
     LeftDrive.stop()
     RightDrive.stop()
 
@@ -125,11 +111,13 @@ def when_started1():
     LeftDrive.set_position(0, DEGREES)
     RightDrive.set_position(0, DEGREES)
     wait(0.2, SECONDS)
-    LeftDrive.spin_for(REVERSE, 415, DEGREES, wait=False)
-    RightDrive.spin_for(REVERSE, 415, DEGREES)
+    LeftDrive.spin_for(REVERSE, 450, DEGREES, wait=False)
+    RightDrive.spin_for(REVERSE, 450, DEGREES)
     wait(0.2, SECONDS)
-    GyroTurnV2_Heading_Velocity_Momentum(270, 50, 10)
+    GyroTurnV2_Heading_Velocity_Momentum(270, 25, 10)
     wait(0.3, SECONDS)
+    LeftDrive.set_velocity(50, PERCENT)
+    RightDrive.set_velocity(50, PERCENT)
 
 def onevent_buttonRight_pressed_0():
     global LeftDriveSpeed, RightDriveSpeed, ShootingSpeed, message1
@@ -177,33 +165,44 @@ def PStraight_distance_heading_velocity_kp(PStraight_distance_heading_velocity_k
 
 # execution
 
-
-motor_speed = 50
 def MoveForwardAndBackward():
-    while True:
+    Conveyor.set_velocity(100, PERCENT)
+    LeftDrive.set_velocity(45, PERCENT)
+    RightDrive.set_velocity(45, PERCENT)
+    counter = 0
+    while True:      
+        # move forward
+        LeftDrive.spin_for(FORWARD, 2.85, TURNS, wait=False)
+        RightDrive.spin_for(FORWARD, 2.85, TURNS)
+
         #Scoring First Ball
-        Conveyor.set_timeout(3, SECONDS)
+        Conveyor.set_timeout(4,SECONDS)
         Conveyor.spin(FORWARD)
-        wait(3, SECONDS)
+        wait(4,SECONDS)
         Conveyor.stop()
+        if counter < 1:
+            for i in range(3):
+                Conveyor.set_velocity(100, PERCENT)
+                LeftDrive.set_velocity(50, PERCENT)
+                RightDrive.set_velocity(50, PERCENT)
+                LeftDrive.spin_for(REVERSE, 0.12, TURNS, wait=False)
+                RightDrive.spin_for(REVERSE, 0.12, TURNS)
+                wait(20,MSEC)
+                LeftDrive.spin_for(FORWARD, 0.12, TURNS, wait=False)
+                RightDrive.spin_for(FORWARD, 0.12, TURNS)
+        # Conveyor.set_velocity(70, PERCENT)
+        # LeftDrive.set_velocity(70, PERCENT)
+        # RightDrive.set_velocity(70, PERCENT)
 
         # moving backwards
-        LeftDrive.spin_for(REVERSE, 2.5, TURNS, wait=False)
-        RightDrive.spin_for(REVERSE, 2.5, TURNS, wait = False)
+        LeftDrive.spin_for(REVERSE, 2.85, TURNS, wait=False)
+        RightDrive.spin_for(REVERSE, 2.85, TURNS, wait = False)
         # pickup ball, start conveyor for 1s
         Conveyor.set_timeout(4, SECONDS)
         Conveyor.spin(FORWARD)
         wait(4, SECONDS)
         Conveyor.stop()
-
-        # move forward
-        LeftDrive.spin_for(FORWARD, 2.5, TURNS, wait=False)
-        RightDrive.spin_for(FORWARD, 2.5, TURNS)
-        # throw the ball, start conveyor for1s
-        Conveyor.set_timeout(2,SECONDS)
-        Conveyor.spin(FORWARD)
-        wait(2, SECONDS)
-        Conveyor.stop()
+        counter += 1
 
 
 def reset_encoders():
@@ -218,84 +217,5 @@ brain.buttonCheck.pressed(onevent_buttonCheck_pressed_0)
 # add 15ms delay to make sure events are registered correctly.
 wait(15, MSEC)
 
-#when_started1()
-global myVariable, error, output
-gyro_11.calibrate(GyroCalibrationType.NORMAL)
-MoveForwardAndBackward()
-
-'''
-def GyroTurn_heading_velocity_momentum(GyroTurn_heading_velocity_momentum__heading, GyroTurn_heading_velocity_momentum__velocity, GyroTurn_heading_velocity_momentum__momentum):
-    global LeftDriveSpeed, RightDriveSpeed, ShootingSpeed, message1
-    if GyroTurn_heading_velocity_momentum__heading > gyro_7.rotation():
-        while GyroTurn_heading_velocity_momentum__heading - GyroTurn_heading_velocity_momentum__momentum > gyro_7.rotation():
-            LeftDrive.set_velocity(GyroTurn_heading_velocity_momentum__velocity, PERCENT)
-            RightDrive.set_velocity(GyroTurn_heading_velocity_momentum__velocity, PERCENT)
-            LeftDrive.spin(REVERSE)
-            RightDrive.spin(FORWARD)
-            wait(20, MSEC)
-    else:
-        while GyroTurn_heading_velocity_momentum__heading + GyroTurn_heading_velocity_momentum__momentum < gyro_7.rotation():
-            LeftDrive.set_velocity(GyroTurn_heading_velocity_momentum__velocity, PERCENT)
-            RightDrive.set_velocity(GyroTurn_heading_velocity_momentum__velocity, PERCENT)
-            LeftDrive.spin(FORWARD)
-            RightDrive.spin(REVERSE)
-            wait(20, MSEC)
-    LeftDrive.stop()
-    RightDrive.stop()
-    LeftDrive.set_velocity(LeftDriveSpeed, PERCENT)
-    RightDrive.set_velocity(RightDriveSpeed, PERCENT)
-
-def Arc_Forward_Distance_Velocity_Offset_Direction(Arc_Forward_Distance_Velocity_Offset_Direction__Distance, Arc_Forward_Distance_Velocity_Offset_Direction__Velocity, Arc_Forward_Distance_Velocity_Offset_Direction__Offset, Arc_Forward_Distance_Velocity_Offset_Direction__Direction):
-    global LeftDriveSpeed, RightDriveSpeed, ShootingSpeed, message1
-    if Arc_Forward_Distance_Velocity_Offset_Direction__Direction == 1:
-        LeftDrive.set_velocity((Arc_Forward_Distance_Velocity_Offset_Direction__Velocity - Arc_Forward_Distance_Velocity_Offset_Direction__Offset), PERCENT)
-        RightDrive.set_velocity(Arc_Forward_Distance_Velocity_Offset_Direction__Velocity, PERCENT)
-    else:
-        LeftDrive.set_velocity(Arc_Forward_Distance_Velocity_Offset_Direction__Velocity, PERCENT)
-        RightDrive.set_velocity((Arc_Forward_Distance_Velocity_Offset_Direction__Velocity - Arc_Forward_Distance_Velocity_Offset_Direction__Offset), PERCENT)
-    LeftDrive.spin(FORWARD)
-    RightDrive.spin(FORWARD)
-    wait(Arc_Forward_Distance_Velocity_Offset_Direction__Distance, SECONDS)
-    LeftDrive.stop()
-    RightDrive.stop()
-    LeftDrive.set_velocity(LeftDriveSpeed, PERCENT)
-    RightDrive.set_velocity(RightDriveSpeed, PERCENT)
-
-def Arc_Backward_Distance_Velocity_Offset_Direction(Arc_Backward_Distance_Velocity_Offset_Direction__Distance, Arc_Backward_Distance_Velocity_Offset_Direction__Velocity, Arc_Backward_Distance_Velocity_Offset_Direction__Offset, Arc_Backward_Distance_Velocity_Offset_Direction__Direction):
-    global LeftDriveSpeed, RightDriveSpeed, ShootingSpeed, message1
-    if Arc_Backward_Distance_Velocity_Offset_Direction__Direction == 1:
-        LeftDrive.set_velocity((Arc_Backward_Distance_Velocity_Offset_Direction__Velocity - Arc_Backward_Distance_Velocity_Offset_Direction__Offset), PERCENT)
-        RightDrive.set_velocity(Arc_Backward_Distance_Velocity_Offset_Direction__Velocity, PERCENT)
-    else:
-        LeftDrive.set_velocity(Arc_Backward_Distance_Velocity_Offset_Direction__Velocity, PERCENT)
-        RightDrive.set_velocity((Arc_Forward_Distance_Velocity_Offset_Direction__Velocity - Arc_Forward_Distance_Velocity_Offset_Direction__Offset), PERCENT)
-    LeftDrive.spin(REVERSE)
-    RightDrive.spin(REVERSE)
-    wait(Arc_Forward_Distance_Velocity_Offset_Direction__Distance, SECONDS)
-    LeftDrive.stop()
-    RightDrive.stop()
-    LeftDrive.set_velocity(LeftDriveSpeed, PERCENT)
-    RightDrive.set_velocity(RightDriveSpeed, PERCENT)
-
-def when_started1():
-    global LeftDriveSpeed, RightDriveSpeed, ShootingSpeed, message1
-    gyro_7.calibrate(GyroCalibrationType.NORMAL)
-    LeftDriveSpeed = 95
-    RightDriveSpeed = 95
-    LeftDrive.set_velocity(LeftDriveSpeed, PERCENT)
-    RightDrive.set_velocity(RightDriveSpeed, PERCENT)
-    LeftDrive.set_stopping(HOLD)
-    RightDrive.set_stopping(HOLD)
-    touchled_5.set_color(Color.BLUE)
-
-def onevent_buttonCheck_pressed_0():
-    global LeftDriveSpeed, RightDriveSpeed, ShootingSpeed, message1
-    pass
-
-# system event handlers
-brain.buttonCheck.pressed(onevent_buttonCheck_pressed_0)
-# add 15ms delay to make sure events are registered correctly.
-wait(15, MSEC)
-
 when_started1()
-'''
+MoveForwardAndBackward()
