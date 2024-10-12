47f770afe451b2542a424cebbb03c45a80ccfad0
Xiaoran Zhang <xiaoranzhang11@gmail.com>
2024-10-11 17:45:34 -0700

    add new project

---
+++ FiveGuysAutomation/src/main.py
@@ -0,0 +1,16 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       xiaoranzhang                                                 #
+# 	Created:      10/11/2024, 5:42:24 PM                                       #
+# 	Description:  IQ2 project                                                  #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello IQ2")
